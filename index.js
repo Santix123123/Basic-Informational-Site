862a9a0562c35a004e67ab9554d05900d39d33c5
Lorenzo Santinelli <lorenzo.santigolf@gmail.com>
2025-04-02 07:07:42 +0200

    Created Big css file for the html files

---
+++ index.js
@@ -0,0 +1,54 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') filePath = './index.html';
+    if (filePath === './about') filePath = './about.html';
+    if (filePath === './contact-me') filePath = './contact-me.html';
+
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    // Set content type based on file extension
+    switch (extname) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+    }
+
+    fs.readFile(path.join(__dirname, filePath), (err, content) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                fs.readFile(path.join(__dirname, '404.html'), (err404, content404) => {
+                    res.writeHead(404, { 'Content-Type': 'text/html' });
+                    res.end(content404, 'utf8');
+                });
+            } else {
+                res.writeHead(500);
+                res.end(`Server Error: ${err.code}`);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf8');
+        }
+    });
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
